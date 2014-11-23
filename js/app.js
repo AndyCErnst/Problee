e8bb4961875451f9d440a0f78538e7f6c4bcb244
Andy Ernst <AndyCErnst@gmail.com>
2014-11-22 18:09:15 -0800

    click to return snippet

--- js/app.js
+++ js/app.js
@@ -5,23 +5,31 @@ var probleeApp = angular.module('probleeApp', ['ngDraggable']);
 probleeApp.controller('probleeDragDropController', ['$scope', '$http',
   function($scope, $http) {
     $scope.introText = 'intro text';
-    $scope.problem = {fields: []};
+    $scope.fields = [];
     $scope.wordBank = ['"hello"','return','var','other thing'];
 
-    $scope.onDropComplete0=function(data,evt){
-      console.log(evt);
-      $scope.problem.fields[0] = data;
-    }
+    $scope.onDropComplete=function(fieldNum,data,evt){
+      $scope.fields[fieldNum] = data;
+    };
+    $scope.onClick=function(fieldNum){
+      var snippet = $scope.fields.splice(fieldNum, 1);
+      console.log(snippet);
+      $scope.wordBank.push(snippet[0]);
+    };
 
-    $scope.onDropComplete1=function(data,evt){
-      $scope.problem.fields[1] = data;
-    }
+    $scope.onDragSuccessFields=function(data,evt){
+      var index = $scope.fields.indexOf(data);
+      if (index > -1) {
+        $scope.fields.splice(index, 1);
+      }
+    };
+
     $scope.onDragSuccessBank=function(data,evt){
       var index = $scope.wordBank.indexOf(data);
       if (index > -1) {
         $scope.wordBank.splice(index, 1);
       }
-    }
+    };
   }
 
   ]);
