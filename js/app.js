b4f5e5938489121f55be432657abfd1a9bc10859
Andy Ernst <AndyCErnst@gmail.com>
2014-11-22 17:31:03 -0800

    get basic function/data working

--- js/app.js
+++ js/app.js
@@ -5,10 +5,22 @@ var probleeApp = angular.module('probleeApp', ['ngDraggable']);
 probleeApp.controller('probleeDragDropController', ['$scope', '$http',
   function($scope, $http) {
     $scope.introText = 'intro text';
-    $scope.problem = {};
+    $scope.problem = {fields: []};
     $scope.wordBank = ['"hello"','return','var','other thing'];
-    $scope.onDropComplete=function(data,evt){
-      console.log("drop success, data:", data, evt);
+
+    $scope.onDropComplete0=function(data,evt){
+      console.log(evt);
+      $scope.problem.fields[0] = data;
+    }
+
+    $scope.onDropComplete1=function(data,evt){
+      $scope.problem.fields[1] = data;
+    }
+    $scope.onDragSuccessBank=function(data,evt){
+      var index = $scope.wordBank.indexOf(data);
+      if (index > -1) {
+        $scope.wordBank.splice(index, 1);
+      }
     }
   }
 
